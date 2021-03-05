a77389828bdcdbb350e4a8880a5ed0760c06c069
Akhil Mohan <mohanakumar0903@gmail.com>
2021-03-05 09:12:17 +0530

    Added Login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASPMVCDemo.Models;
+
+namespace ASPMVCDemo.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        ReleaseManagementContext context = new ReleaseManagementContext();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public String Index(LoginDetails loginDetails)
+        {
+            var data = context.LoginDetails.FirstOrDefault(x => x.UserID == loginDetails.UserID);
+
+            if (data != null)
+            {
+
+                if (data.Password == loginDetails.Password)
+                {
+                    return "Welcome " + data.UserRole;
+                }
+                else
+                {
+                    return "User ID or Password Incorrect";
+                }
+
+
+
+            }
+            else
+                return "Login Failed";
+        }
+
+    }
+}
